1c9d50a49e042419eb5d9804c1c5e5427e15dc76
mcruggiero <michael@mcruggiero.com>
2020-04-11 09:14:01 -0400

    added items

--- 001_RBIF-100-1DL/Week_2/motif_generator.py
+++ 001_RBIF-100-1DL/Week_2/motif_generator.py
@@ -1,36 +1,43 @@
 #!/usr/bin/env python
 
-# Numpy is a common library, I will use it here,
-import numpy as np
+import random
 
 ###
-# Until I get the motif list, I am going to cook myself something up
-# I am not going to use numpy for the code below since I am going to translate
-# the Python code to Bash.
+# If you want a little python motif_generator to test out your bash scripts
 ###
 
 class Motif_Generator:
-    def __init__(self, number, min_length, max_length):
+    
+    # The first thing this class will do is run the __init__
+    def __init__(self, number, min_length, max_length):    
+        # Make an empty list of motifs
         motifs = []
         
+        # Use the motif_generator function to populate the motif list 
         for _ in range(number):
             motifs.append(self.motif_generator(min_length, max_length))
         
-        # Write the motifs to a file
-        
+        # Write the motif list to file to a file
         with open('motifs.txt', 'w') as f:
             for motif in motifs:
                 f.write("{}\n".format(motif))
         
     def motif_generator(self, min_length, max_length):
-        
         # Set the size of the random item
-        size = np.random.randint(low = min_length, high = max_length)
+        size = random.randint(min_length, max_length)
         
-        # Populate that list
-        nucleotides = "".join(np.random.choice(["a", "c", "g", "t"], size))
+        # Make a blank list for the nucleotides
+        nucleotides = []
+        
+        # Choose a nucleotide and append it to the list
+        for _ in range(size):
+            nucleotides.append(random.choice(["a", "c", "g", "t"]))
+
+        # Turn that list into a string
+        nucleotides = "".join(nucleotides)
+        
         return nucleotides
     
 if __name__ == '__main__':
-    # Not really sure how large to make each motif, I suppose it doesn't matter.
+    # When the class is called, it will make 10 motifs of sizes between 7 and 9
     Motif_Generator = Motif_Generator(10, 7, 9)
