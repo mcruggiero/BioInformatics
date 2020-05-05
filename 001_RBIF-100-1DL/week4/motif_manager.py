84cbf5a4946cf129b0692fef9462b286674499d6
mcruggiero <michael@mcruggiero.com>
2020-05-05 03:52:50 +0000

    added items

--- 001_RBIF-100-1DL/week4/motif_manager.py
+++ 001_RBIF-100-1DL/week4/motif_manager.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 
-import re
-import os
+import os, shutil
 import pandas as pd
 from itertools import combinations, permutations
 
 def copy_exomes():
-    print("copying exomes")
+    # Read in tab deliniated file
+    df = pd.read_csv("clinical_data.txt", sep='\t', engine='python')
+    
+    # Filter out by needed terms
+    mover = df[(20 <= df['Diamater (mm)']) & (df['Diamater (mm)'] <= 30)]
+    print("The following entries are of interest:")
+    print(mover)
+    
+    # Make Directory Structure
+    if not os.path.exists("exomes"):
+        os.mkdir("exomes")
+    else:
+        shutil.rmtree("exomes")
+        os.mkdir("exomes")
+    
+    # Check to see if path exists, it it does, remove and create
+    if not os.path.exists("exomes"):
+        os.mkdir("exomes")
+    else:
+        shutil.rmtree("exomes")
+        os.mkdir("exomes")
+
+    # Copy files
+    for i in mover["code_name"].tolist():
+        print("copying {}.fasta file".format(i))
+        shutil.copy2("exomes_given/{}.fasta".format(i), 
+                     "exomes/{}.fasta".format(i))
