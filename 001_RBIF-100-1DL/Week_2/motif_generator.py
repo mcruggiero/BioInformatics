aa0abf9f0ea9a80b4fe0ccf1218af393be632f73
mruggiero <mruggiero@pop-os.localdomain>
2020-05-06 22:05:17 -0400

    added changes

--- 001_RBIF-100-1DL/Week_2/motif_generator.py
+++ 001_RBIF-100-1DL/Week_2/motif_generator.py
@@ -1,43 +1,35 @@
-#!/usr/bin/env python
 
+# The built in random generator
 import random
 
 ###
-# If you want a little python motif_generator to test out your bash scripts
+# Until I get the motif list, I am going to cook myself something up
+# I am not going to use numpy for the code below since I am going to translate
+# the Python code to Bash.
 ###
 
 class Motif_Generator:
-    
-    # The first thing this class will do is run the __init__
-    def __init__(self, number, min_length, max_length):    
-        # Make an empty list of motifs
+    def __init__(self, number, min_length, max_length):
         motifs = []
         
-        # Use the motif_generator function to populate the motif list 
         for _ in range(number):
             motifs.append(self.motif_generator(min_length, max_length))
         
-        # Write the motif list to file to a file
+        # Write the motifs to a file
+        
         with open('motifs.txt', 'w') as f:
             for motif in motifs:
                 f.write("{}\n".format(motif))
         
     def motif_generator(self, min_length, max_length):
+        
         # Set the size of the random item
-        size = random.randint(min_length, max_length)
+        size = random.randint(low = min_length, high = max_length)
         
-        # Make a blank list for the nucleotides
-        nucleotides = []
-        
-        # Choose a nucleotide and append it to the list
-        for _ in range(size):
-            nucleotides.append(random.choice(["a", "c", "g", "t"]))
-
-        # Turn that list into a string
-        nucleotides = "".join(nucleotides)
-        
+        # Populate that list
+        nucleotides = "".join(random.choice(["a", "c", "g", "t"], size))
         return nucleotides
     
 if __name__ == '__main__':
-    # When the class is called, it will make 10 motifs of sizes between 7 and 9
+    # Not really sure how large to make each motif, I suppose it doesn't matter.
     Motif_Generator = Motif_Generator(10, 7, 9)
